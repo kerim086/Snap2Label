6995524ed9fde25bd8551f1c10d884314a69e409
kerim086 <147099168+kerim086@users.noreply.github.com>
2025-12-14 18:26:09 +0100

    Add screenshot loop with ESC key exit
    Introduces a loop that continuously grabs screenshots using mss until the ESC key is pressed, utilizing the keyboard module for key detection.

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 from mss import mss
-...
+import keyboard
+
+with mss() as sct:
+    while True:
+        if keyboard.is_pressed('ESC'):
+            break
+
+        screenshot = sct.grab(sct.monitors[0])
